28c12e23c65622dee6695b411acf6736fb180482
Samson Akanbi <samson.akanbi@outlook.com>
2017-03-25 05:27:06 +0000

    Added a user_id column to Post

--- db/migrate/20170325052521_add_column_user_id_to_post.rb
+++ db/migrate/20170325052521_add_column_user_id_to_post.rb
@@ -1,4 +1,4 @@
-class AddColumnUserId < ActiveRecord::Migration[5.0]
+class AddColumnUserIdToPost < ActiveRecord::Migration[5.0]
   def change
     add_column :posts, :user_id, :integer
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324165258) do
+ActiveRecord::Schema.define(version: 20170325052521) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "comment_id"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
