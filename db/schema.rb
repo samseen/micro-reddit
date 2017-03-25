4d115fece340abb03ed9993b1e61798536b8bb65
Samson Akanbi <samson.akanbi@outlook.com>
2017-03-25 04:22:01 +0000

    Added validations to Post and User models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324161144) do
+ActiveRecord::Schema.define(version: 20170324165258) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
